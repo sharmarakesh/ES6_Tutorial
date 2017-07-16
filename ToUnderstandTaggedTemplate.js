20f7b88ce1c0da971dfeeb33d39c79f9b7983a47
Rakesh Sharma <rakeshsharma.sharma@gmail.com>
2017-07-16 21:04:42 +0530

    To Understand Tagged Template see how substitutions works

---
+++ ToUnderstandTaggedTemplate.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function passthru(literals, ...substitutions) {
+    let result = "";
+
+    // run the loop only for the substitution count
+    for (let i = 0; i < substitutions.length; i++) {
+        result += literals[i];
+        result += substitutions[i];
+    }
+
+    // add the last literal
+    result += literals[literals.length - 1];
+
+    return result;
+}
+
+let count = 10,
+    price = 0.25,
+    message = passthru `${count} items cost $${(count * price).toFixed(2)}.`;
+
+console.log(message); // "10 items cost $2.50."
