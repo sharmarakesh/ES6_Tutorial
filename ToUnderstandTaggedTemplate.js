c62efa1c489fcec8d61d3cb341110a22c5ba1063
Rakesh Sharma <rakeshsharma.sharma@gmail.com>
2017-07-16 21:17:42 +0530

    added comment

--- ToUnderstandTaggedTemplate.js
+++ ToUnderstandTaggedTemplate.js
@@ -20,3 +20,5 @@ let count = 10,
     message = passthru `${count} items cost $${(count * price).toFixed(2)}.`;
 
 console.log(message); // "10 items cost $2.50."
+
+//This is an example to understand how template tag works internally.
